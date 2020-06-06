a2412cc2273b1463084a6f0545e4acc3bdf511a1
Sana Ebadi <sanaebadi97@gmail.com>
2020-06-06 13:11:01 -0400

    added dummy data in NetworkService.kt

--- app/src/main/java/info/sanaebadi/dependencymanagement/data/remote/NetworkService.kt
+++ app/src/main/java/info/sanaebadi/dependencymanagement/data/remote/NetworkService.kt
@@ -1,2 +1,13 @@
 package info.sanaebadi.dependencymanagement.data.remote
 
+import android.content.Context
+
+/**
+ * Dummy class to simulate the actual NetworkService using Retrofit or OkHttp etc
+ */
+
+class NetworkService(context: Context) {
+    private val apiKey = "SOME_API_KEY"
+    private val getDummyData: String
+        get() = "NETWORK_DUMMY_DATA"
+}
