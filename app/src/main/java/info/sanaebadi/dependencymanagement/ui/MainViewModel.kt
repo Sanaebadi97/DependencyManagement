a110040275d66c78edc035efd42bd3dbe8b87297
Sana Ebadi <sanaebadi97@gmail.com>
2020-06-06 13:31:42 -0400

    created instance for MainViewModel.kt and use getDummyData and set text on tvData . added ViewBinding !

--- app/src/main/java/info/sanaebadi/dependencymanagement/ui/MainViewModel.kt
+++ app/src/main/java/info/sanaebadi/dependencymanagement/ui/MainViewModel.kt
@@ -1,9 +1,12 @@
 package info.sanaebadi.dependencymanagement.ui
 
+import android.content.Context
 import info.sanaebadi.dependencymanagement.data.local.DatabaseService
 import info.sanaebadi.dependencymanagement.data.remote.NetworkService
 
-class MainViewModel(val databaseService: DatabaseService, val networkService: NetworkService) {
+class MainViewModel(context: Context) {
+    private val databaseService = DatabaseService(context)
+    private val networkService = NetworkService(context)
     val getSomeData: String
         get() = "${databaseService.getDummyData} : : ${networkService.getDummyData}}"
 }
