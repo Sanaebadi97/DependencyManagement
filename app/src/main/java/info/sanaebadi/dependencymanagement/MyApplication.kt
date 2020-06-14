828c064a41271245fc3b709c2ea259fc67c09b3d
Sana Ebadi <sanaebadi97@gmail.com>
2020-06-14 12:31:46 -0400

    added @Inject on dependencies

--- app/src/main/java/info/sanaebadi/dependencymanagement/MyApplication.kt
+++ app/src/main/java/info/sanaebadi/dependencymanagement/MyApplication.kt
@@ -4,10 +4,14 @@ import android.app.Application
 import info.sanaebadi.dependencymanagement.data.local.DatabaseService
 import info.sanaebadi.dependencymanagement.data.remote.NetworkService
 import info.sanaebadi.dependencymanagement.di.DependencyComponent
+import javax.inject.Inject
 
 class MyApplication : Application() {
 
+    @Inject
     var networkService: NetworkService? = null
+
+    @Inject
     var databaseService: DatabaseService? = null
 
 
